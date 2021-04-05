53ddb5615987ec43ca48b3e748f41ab2db8a6ccb
Alexander Römer <alexroemerdeveloper@gmail.com>
2021-04-05 18:36:36 +0200

    Initial Commit

---
+++ ArrowAnimation WatchKit Extension/ArrowAnimationApp.swift
@@ -0,0 +1,19 @@
+//
+//  ArrowAnimationApp.swift
+//  ArrowAnimation WatchKit Extension
+//
+//  Created by Alexander Römer on 05.04.21.
+//
+
+import SwiftUI
+
+@main
+struct ArrowAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
---
+++ ArrowAnimation WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ArrowAnimation WatchKit Extension
+//
+//  Created by Alexander Römer on 05.04.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
